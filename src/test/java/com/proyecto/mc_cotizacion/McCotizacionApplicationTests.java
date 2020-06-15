c6301494e8f663fd6ea6cb34877b80074384eec5
Gary Alberto Chavez Jimenez <gchavezj@everis.com>
2020-06-15 14:04:50 -0500

    microservicio

---
+++ src/test/java/com/proyecto/mc_cotizacion/McCotizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.mc_cotizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McCotizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
