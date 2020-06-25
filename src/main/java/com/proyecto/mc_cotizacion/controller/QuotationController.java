f580d9cce0621edc9f809803baf01717ff5ad39f
Gary <garychavezj@gmail.com>
2020-06-24 22:23:09 -0500

    Fix: actualiza @ApiOperation de la clase QuotationController

--- src/main/java/com/proyecto/mc_cotizacion/controller/QuotationController.java
+++ src/main/java/com/proyecto/mc_cotizacion/controller/QuotationController.java
@@ -34,35 +34,40 @@ public class QuotationController {
 	QuotationService quotationService;
 	
 	@PostMapping
-	@ApiOperation(value = "Registrar Cotizacion", notes = "Metodo Post para registrar  Cotizacion" )
+	@ApiOperation(value = "Metodo a traves del cual se envia la informacion de la  Cotizacion que  sera registrada dentro de la base de datos",
+			notes = "Para el registro de la cotizacion , sera necesario el llenado de todo los campos , a excepcion de los Id´s los cuales seran generados automaticamente " )
 	public Completable save( @Valid @RequestBody QuotationRequest model) {
 		log.info("Envio de parametros");
 		return  quotationService.save(model);
 	}
 
 	@PutMapping("/{id}")
-	@ApiOperation(value = "Actualizar Cotizacion", notes = "Metodo Put para actualizar  Cotizacion" )
+	@ApiOperation(value = "Metodo a traves del cual te permite cambiar datos especificos  de la Cotizacion, permitiendo asi contar con una base de datos actualizada",
+			notes = "Es necesario el envio del Id de la cotizacion , junto a sus campos debidamente actualizados con la informacion mas reciente" )
 	public Completable update(@RequestBody QuotationRequest model) {
 		log.info("Actualizacion de parametros");
 		return quotationService.update(model);
 	}
 	
 	@GetMapping
-	@ApiOperation(value = "Traer Cotizaciones", notes = "Metodo Get para traer todas las  Cotizacion" )
+	@ApiOperation(value = "Metodo a traves del cual te permite obtener la informacion de  todas las  Cotizacion que se encuentran almacenadas en la base de datos",
+			notes = "Permite obtener todas as cotizaciones" )
 	public Observable<QuotationResponse> findAll(){
 		log.info("Obtencion de datos");
 		return quotationService.findAll();
 	}
 
 	@GetMapping(Constants.ID)
-	@ApiOperation(value = "Traer Cotizacion por Id", notes = "Metodo Get para traer una Cotizacion por Id" )
+	@ApiOperation(value = "Metodo a traves del cual te permite obtener los datos de una Cotizacion en especifico , a traves de la busqueda de su identificador (id)  ",
+			notes = "Es necesario el envio del Id de la cotizacion ,para obtener  sus datos" )
 	public Single<QuotationResponse> getById(@PathVariable ("id") Long id) {
 		log.info("Obtencion de datos por id");
 		return quotationService.getById(id);		
 	}
 
 	@GetMapping(Constants.STATUS)
-	@ApiOperation(value = "Traer Cotizaciones por estatus", notes = "Metodo Get para traer una lista de Cotizaciones por estatus" )
+	@ApiOperation(value = "Metodo a traves del cual te permite obtener los datos de una o varias  Cotizaciones , a traves de su Status",
+			notes = "Es necesario el envio del Status de la cotizacion ,para obtener  los datos de una o varias  Cotizaciones , a traves de su Status" )
 	public Observable<QuotationResponse> findStatus(@Valid @PathVariable ("status") QuotationStatus status) {
 		log.info("Obtencion de datos por status");
 		return quotationService.findStatus(status);
